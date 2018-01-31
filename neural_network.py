8114f1bda20de2fc71744addff9f1f58a6c08eb3
DECELLE Remi <remi.decelle@telecomnancy.eu>
2018-01-31 15:11:29 +0100

    Neural network seems done

--- neural_network.py
+++ neural_network.py
@@ -3,16 +3,16 @@ import math as math
 import numpy as np 
 import csv as csv
 import json
-
-# For style
-import pprint
+import matplotlib.pyplot as plt
 
 NCN = float('Inf')
 
 # Load network.json
 def load_network():
 	with open('./network.json', 'rb'):
-		network = json.load(open('./network.json', 'rb'))['network']
+		alls = json.load(open('./network.json', 'rb'))
+		network = alls['network']
+		shared = alls['shared']
 		for k, layer in enumerate(network):
 			for i, neuron in enumerate(layer):
 				for key in neuron['weights'].keys():
@@ -22,7 +22,10 @@ def load_network():
 							neuron['weights'][key][j] = NCN
 				if neuron['function'] == "None":
 					neuron['function'] = None
-	return network
+		for i, row in enumerate(shared):
+			for j, link in enumerate(row):
+				shared[i][j] = tuple(int(i) for i in str(link)[1:-1].split(','))
+	return network, shared
 
 # Load file
 def load_file():
@@ -40,15 +43,11 @@ def load_file():
 
 # Cross Validation
 def cross_validation_split(dataset, n_folds):
-	dataset_split = list()
-	dataset_copy = list(dataset)
+	dataset_split = []
+	dataset_copy = dataset
 	fold_size = int(len(dataset) / n_folds)
 	for i in range(n_folds):
-		fold = list()
-		while len(fold) < fold_size:
-			index = random.randrange(len(dataset_copy))
-			fold.append(dataset_copy.pop(index))
-		dataset_split.append(fold)
+		dataset_split.append(dataset_copy[i*fold_size:(i+1)*fold_size])
 	return dataset_split
 
 # Calculate accuracy percentage
@@ -147,41 +146,40 @@ def update_weights(network, inputs, alpha, momentum, shared):
 		for i, neuron in enumerate(layer):
 			# For each i-th layer connected with the neuron
 			for key in neuron['weights']:
-				#if neuron['shared']:
-					# Do something
-				#	pass 
 				# If bias
 				if key == 'bias':
+					#neuron['weights'][key] = neuron['weights'][key] - alpha * neuron['epsilon']
 					neuron['weights'][key] += alpha * neuron['epsilon']
 				# Else weight
 				else:
-					# For each neuron in i-th layer potential connected with k-th layer
+					# For each neuron in i-th layer potentially connected with k-th layer
 					for j in range(len(neuron['weights'][key])):
+						# If there is no connection continue
 						if neuron['weights'][key][j] == NCN: continue
+						# If it's the output layer
 						if int(key) == 0:
-							mu = (1.0 - momentum) * neuron['weights'][key][j] + momentum * inputs[j] * neuron['epsilon']
+							mu = momentum * neuron['weights'][key][j] + inputs[j] * neuron['epsilon']
+							#neuron['weights'][key][j] = neuron['weights'][key][j] - alpha * mu
 							neuron['weights'][key][j] += alpha * mu
 						else:
-							
 							sum_share = 0.0
 							# If the neuron is shared : Shared weights
-							for share in shared:
-								if (k, i, int(key), j) in share: 
-									for couple in share:
-										sum_share += network[k][i]['output'] * neuron['epsilon']
-								if (int(key), j, k, i) in share:
-									for couple in share:
-										sum_share += network[k][i]['output'] * neuron['epsilon']
+							if 'shared' in neuron['weights']:
+								for share in shared:
+									if (k, i, int(key), j) in share: 
+										for couple in share:
+											sum_share += network[k][i]['output'] * neuron['epsilon']
+									elif (int(key), j, k, i) in share:
+										for couple in share:
+											sum_share += network[k][i]['output'] * neuron['epsilon']
 							#mu = (1.0 - momentum) * neuron['weights'][key][j] + momentum * network[int(key)-1][j]['output'] * neuron['epsilon']
-							mu = (1.0 - momentum) * neuron['weights'][key][j] + momentum * sum_share
+							mu = momentum * neuron['weights'][key][j] +  sum_share
+							#neuron['weights'][key][j] = neuron['weights'][key][j] - alpha * mu
 							neuron['weights'][key][j] += alpha * mu
 
 # Cross entropy error function
 def cross_entropy(excepted, predict):
-	try:
-		return - excepted * math.log((excepted / predict) + 10e-8, 2)
-	except ValueError:
-		print excepted, predict 
+	return - excepted * math.log(predict, 10)
 
 # MSE
 def mean_squared(x, y):
@@ -192,35 +190,84 @@ def decay_learning_rate(alpha, tau, t):
 	return alpha * tau / (tau + t) if tau > 0 else alpha
 
 # Train the network
-def train_network(network, trainset, shared, alpha, momentum, tau, num_iter, n_outputs):
-	for epoch in range(num_iter):
-		sum_error = 0
+def train(network, trainset, validset, testset, shared, alpha, momentum, tau, num_iter, n_outputs):
+	list_valid_error = list()
+	list_train_error = list()
+	list_test_error = list()
+
+	mean_valid_error = float('Inf')
+	valid_error = 999.0
+	epoch = 0
+
+	# Early Stopping
+	# We don't use mean because it seems that validation error can decrease for a long time (over 10000 iterations)
+	while float('{0:.5}'.format(valid_error)) < float('{0:.5}'.format(mean_valid_error)) and epoch < num_iter:
+		
+		# Update value for error and early stopping
+		mean_valid_error = valid_error
+		train_error = 0.0
+		valid_error = 0.0
+		test_error = 0.0
+
+		# Decay Learning Rate
 		current_alpha = decay_learning_rate(alpha, tau, epoch)
-		#np.random.shuffle(trainset)
+
+		# Stochastic Gradient Descent
+		# For each traning set example do
 		for inputs in trainset:
+			# Estimate outputs
+			outputs = softmax(forward_propagate(network, [inputs[:-1]])[-1])
+
+			# Create expected outputs
+			expected = [0.0 for i in range(n_outputs)]
+			expected[int(inputs[-1])-1] = 1.0
+
+			# Sum error according to cross entropy error
+			train_error += sum([cross_entropy(expected[i], outputs[i]) for i in range(n_outputs)])
+			#print cross_entropy(expected[0], outputs[0]), expected[0], outputs[0]
+
+			# Do backward propagation algorithm
+			backward_propagate(network, expected)
+
+			# Update weights and biases
+			update_weights(network, inputs, current_alpha, momentum, shared)
+
+		# Compute Error on validation dataset
+		for inputs in validset:
 			outputs = softmax(forward_propagate(network, [inputs[:-1]])[-1])
 			expected = [0.0 for i in range(n_outputs)]
 			expected[int(inputs[-1])-1] = 1.0
-			#expected = [inputs[-1]]
-			#sum_error += mean_squared(expected[-1], outputs[-1])
-			sum_error += sum([cross_entropy(expected[i], outputs[i]) for i in range(n_outputs)])
-			backward_propagate(network, expected)
-			update_weights(network, inputs, current_alpha, momentum, shared)
-		print "Epoch : {}; lrate = {:.3}; error={:.5}".format(epoch, current_alpha, sum_error)
+			valid_error += sum([cross_entropy(expected[i], outputs[i]) for i in range(n_outputs)])
+
+		# Compute Error on test dataset
+		for inputs in validset:
+			outputs = softmax(forward_propagate(network, [inputs[:-1]])[-1])
+			expected = [0.0 for i in range(n_outputs)]
+			expected[int(inputs[-1])-1] = 1.0
+			test_error += sum([cross_entropy(expected[i], outputs[i]) for i in range(n_outputs)])
+
+		list_valid_error.append(valid_error)
+		list_test_error.append(test_error)
+		list_train_error.append(train_error)
+		epoch += 1
+
+	return list_train_error, list_valid_error, list_test_error
 
 # Compute softmax
 def softmax(inputs):
-	exp_in = map(math.exp, inputs)
+	m = max(inputs)
+	# Prevents exp to overflow
+	exp_in = map(math.exp, map(lambda x : x-m, inputs))
 	return [item / sum(exp_in) for item in exp_in]
 
 # Make a prediction with a network
 def predict(network, inputs):
 	outputs = forward_propagate(network, inputs)
 	return outputs[-1]
-	#return outputs.index(max(outputs))
 
 # Randomly uniform initialization
 def initialize(network, shared):
+
 	for layer in network:
 		for neuron in layer:
 			for connection in neuron['weights'].keys():
@@ -234,54 +281,76 @@ def initialize(network, shared):
 		for t in share:
 			network[t[0]][t[1]]['weights'][str(t[2])][t[3]] = network[share[0][0]][share[0][1]]['weights'][str(share[0][2])][share[0][3]]
 
-random.seed(1)
-dataset = load_file()
-np.random.shuffle(dataset)
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	# Create n-folds
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
 
-shared = [
-	# Convonlutional C1
-	[(0, 0, 0, 0), (0, 1, 0, 1), (0, 2, 0, 2)],
-	[(0, 3, 0, 0), (0, 4, 0, 1), (0, 5, 0, 2)],
-	[(0, 0, 0, 1), (0, 1, 0, 2), (0, 2, 0, 3)],
-	[(0, 3, 0, 1), (0, 4, 0, 2), (0, 5, 0, 3)],
+	# For each
+	for i in range(len(folds)):
+		testset = folds[i]
+		score = list()
+		for j in range(len(folds)):
+			if i == j: continue
+			validset = folds[j]
 
-	# Conconlutional C2
-	[(1, 0, 1, 0), (1, 1, 1, 1)],
-	[(1, 0, 1, 1), (1, 1, 1, 2)],
-	[(1, 0, 1, 3), (1, 1, 1, 4)],
-	[(1, 0, 1, 4), (1, 1, 1, 5)],
-	[(1, 2, 1, 0), (1, 3, 1, 1)],
-	[(1, 2, 1, 1), (1, 3, 1, 2)],
-	[(1, 2, 1, 3), (1, 3, 1, 4)],
-	[(1, 2, 1, 4), (1, 3, 1, 5)],
+			trainset = [folds[x] for x in range(n_folds) if x!=j and x!=i]
 
-	# Shared additional
-	[(2, 0, 0, 0), (3, 0, 0, 0), (4, 0, 0, 0)],
-	[(2, 0, 0, 1), (3, 0, 0, 1), (4, 0, 0, 1)],
-	[(2, 0, 0, 2), (3, 0, 0, 2), (4, 0, 0, 2)],
-	[(2, 0, 0, 3), (3, 0, 0, 3), (4, 0, 0, 3)],
+			actual_trainset = []
+			for n in range(len(trainset)):
+				for row in trainset[n]:
+					actual_trainset.append(row)
 
-	[(2, 1, 0, 0), (3, 1, 0, 0), (4, 1, 0, 0)],
-	[(2, 1, 0, 1), (3, 1, 0, 1), (4, 1, 0, 1)],
-	[(2, 1, 0, 2), (3, 1, 0, 2), (4, 1, 0, 2)],
-	[(2, 1, 0, 3), (3, 1, 0, 3), (4, 1, 0, 3)],
-]
+			# Train & Test
+			predicted = algorithm(actual_trainset, validset, testset, *args)
+			actual = [row[-1] for row in testset]
+			predicted = map(lambda x : x+1, map(np.argmax, predicted))
+			accuracy = accuracy_metric(actual, predicted)
+			score.append(accuracy)
 
-network = load_network()
+		scores.append(score)
+	return scores
 
-np.random.shuffle(dataset)
-learning_rate = 0.05
-tau = 50
-momentum = 1.0
-n_outputs = 3
-epochs = 100
+# Backpropagation Algorithm With Stochastic Gradient Descent
+def train_algorithm(trainset, validset, testset, alpha, momentum, tau, epochs, n_outputs):
+	network, shared = load_network()
+	initialize(network, shared)
+	trains, valids, tests = train(network, trainset, validset, testset, shared, alpha, momentum, tau, epochs, n_outputs)
 
-pp = pprint.PrettyPrinter()
-initialize(network, shared)
+	#fig = plt.figure()
+	#plt.ion()
+	#ax = fig.add_subplot(111)
+	#x = np.linspace(0, len(trains), num=len(trains))
+	#trains = ax.scatter(x, trains, s=5, c='red', marker="+")
+	#valids = ax.scatter(x, valids, s=5, c='green', marker="+")
+	#tests = ax.scatter(x, tests, s=5, c='blue', marker="+")
+	#ax.set_title('Erreur au cours du temps', fontsize=14)
+	#ax.set_xlabel('Nombre iterations')
+	#ax.set_ylabel('Erreur totale')
+	#ax.legend([trains, valids, tests], ["Base apprentissage", "Base validation", "Base test"])
+	#plt.plot()
+	#plt.draw()
+	#plt.pause(15)
 
-train_network(network, dataset, shared, learning_rate, momentum, tau, epochs, n_outputs)
-#for layer in network:
-#	print(layer)
-print predict(network, [[1, 0, 0, 0, None]])
-print predict(network, [[0, 1, 1, 0, None]])
-print predict(network, [[1, 0, 0, 1, None]])
+	predictions = list()
+	for row in testset:
+		prediction = predict(network, [row])
+		predictions.append(prediction)
+	return(predictions)
+
+if __name__ == '__main__':
+	random.seed(1)
+	dataset = load_file()
+	np.random.shuffle(dataset)
+	learning_rate = 0.3
+	tau = 200
+	momentum = 0.00
+	n_outputs = 3
+	epochs = 10000
+	n_folds = 5
+
+	scores = evaluate_algorithm(dataset, train_algorithm, n_folds, learning_rate, momentum, tau, epochs, n_outputs)
+	print('Scores: %s' % scores)
+	for x in scores:
+		print 'Mean Accuracy : {0:4f}'.format(sum(x)/float(len(x)))
